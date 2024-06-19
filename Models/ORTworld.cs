e232dab27dab6756eac78a683bb60ac259bdf4d4
MATÍAS ILÁN BEBCZUK <48242936@ort>
2024-06-19 09:53:49 -0300

    finiquitado

--- Models/ORTworld.cs
+++ Models/ORTworld.cs
@@ -2,7 +2,7 @@ class ORTworld{    public static List < string > ListaDestinos {get; private set
     public static List < string > ListaHoteles {get; private set;} = new List<string>{"hotelBuenosAires.jpg","hotelPorto.jpg", "hotelParis.jpg", "hotelMadrid.jpg", "hotelNewYork.jpeg", "hotelMoscu.jpeg","hotelCasaBlanca.jpg", "hotelMontevideo.jpg", "hotelRoma.jpg", "hotelAtenas.jpeg"};
     public static List< string > ListaAereos {get; private set;} = new List<string>{"AerolineasArgentinas.png", "aeromexico.png", "AmericanAirlines.png", "flybondi.png", "JetSmart.png", "LADE.png", "Lufthansa.png", "qatarairlines.png", "turkishairlines.png", "unitedairlines.png"};
     public static List< string > ListaExcursiones {get; private set;} = new List<string>{"bsasExc.jpeg", "portoExc.jpeg", "parisExc.jpeg", "madridExc.jpeg", "newExc.jpeg", "moscuExc.jpeg", "casabExc.jpeg", "montevExc.jpeg", "romaExc.jpeg", "greciaExc.jpeg"};
-    public static Dictionary<string, Paquete> Paquetes {get; private set;}
+    public static Dictionary<string, Paquete> Paquetes {get; private set;} = new Dictionary<string, Paquete>();
     public static bool IngresarPaquete(string destinoSeleccionado, Paquete paquete) {
         bool existe = Paquetes.Keys.Contains(destinoSeleccionado);
         if(!existe){
